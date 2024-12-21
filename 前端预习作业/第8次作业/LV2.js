6a1fee1a31e5fa6831bdca1f8ac83e254b8c97ac
player32611 <hcb326630@163.com>
2024-12-21 17:57:25 +0800

    第8次作业

--- 前端预习作业/第8次作业/LV2.js
+++ 前端预习作业/第8次作业/LV2.js
@@ -3,7 +3,7 @@ const url = `https://api.github.com/user/repos`
 const res = await fetch(url, {
     method: 'POST',
     headers: {
-        'Authorization': `token API`,//输了真的API就不知到为什么提交不了了
+        'Authorization': `token API`,//输了真的API就不知道为什么提交不了了
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
